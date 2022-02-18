8f567f0cf8c4e6f03ce7653c7f62acf6e788471b
Jermaine Kitson <Jermaine.Kitson@mymhc.ca>
2022-02-18 14:21:46 -0700

    Added Sqiltedatalayer

---
+++ Assignment4/Assignment4/SQLiteDataLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment4
+{
+    public class SQLiteDataLayer
+    {
+        string str = "@Data Source=c:\\IOTdb.db;Version=3;";
+
+        public SQLiteDataLayer()
+        {
+
+        }
+    }
+}
